e6584cb91001525db191c7016cf1d192c351e279
Jordan Puglisi <jpugs1527@yahoo.com>
2018-12-04 16:30:55 -0500

    Update package.json

--- routes/user.js
+++ routes/user.js
@@ -28,7 +28,7 @@ exports.login = function(req, res) {
         console.log(results[0].id);
         req.session.userId = results[0].id;
         req.session.user = results[0];
-        
+
         res.redirect("/home/dashboard");
       } else {
         message = "Wrong Credentials.";
@@ -40,46 +40,52 @@ exports.login = function(req, res) {
   }
 };
 
-exports.signup = function(req, res){
-  message = '';
-  if(req.method == "POST"){
-     var post  = req.body;
-     var name= post.user_name;
-     var pass= post.password;
-     var fname= post.first_name;
-     var lname= post.last_name;
-     var mob= post.mob_no;
+exports.signup = function(req, res) {
+  message = "";
+  if (req.method == "POST") {
+    var post = req.body;
+    var name = post.user_name;
+    var pass = post.password;
+    var fname = post.first_name;
+    var lname = post.last_name;
+    var mob = post.mob_no;
 
-     var sql = "INSERT INTO `users`(`first_name`,`last_name`,`mob_no`,`user_name`, `password`) VALUES ('" + fname + "','" + lname + "','" + mob + "','" + name + "','" + pass + "')";
+    var sql =
+      "INSERT INTO `users`(`first_name`,`last_name`,`mob_no`,`user_name`, `password`) VALUES ('" +
+      fname +
+      "','" +
+      lname +
+      "','" +
+      mob +
+      "','" +
+      name +
+      "','" +
+      pass +
+      "')";
 
-     var query = db.query(sql, function(err, result) {
-
-        message = "Succesfully! Your account has been created.";
-        res.render('signup.ejs',{message: message});
-     });
-
+    var query = db.query(sql, function(err, result) {
+      message = "Succesfully! Your account has been created.";
+      res.render("signup.ejs", { message: message });
+    });
   } else {
-     res.render('signup');
+    res.render("signup");
   }
 };
 
-exports.dashboard = function(req, res, next){
-	
-	var user =  req.session.user,
-	userId = req.session.userId;
-	
-	if(userId == null){
-		res.redirect("/home/login");
-		return;
-	}
-	 
-	 var sql="SELECT * FROM `login_details` WHERE `id`='"+userId+"'";
-	 
-	   db.query(sql, function(err, results){
-		   
-		   console.log(results);
-		   
-		   res.render('profile.ejs', {user:user});	  
-		  
-		});	 
+exports.dashboard = function(req, res, next) {
+  var user = req.session.user,
+    userId = req.session.userId;
+
+  if (userId == null) {
+    res.redirect("/home/login");
+    return;
+  }
+
+  var sql = "SELECT * FROM `login_details` WHERE `id`='" + userId + "'";
+
+  db.query(sql, function(err, results) {
+    console.log(results);
+
+    res.render("profile.ejs", { user: user });
+  });
 };
